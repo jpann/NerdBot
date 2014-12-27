9fd17ed2ed760502b075c395a10d4e5e9a3fd4fa
jpann <ikonmobile@gmail.com>
2014-12-27 11:08:07 -0700

    Add test placeholders.

---
+++ NerdBot/NerdBot.Tests/GroupMeMessenger_Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NerdBot.Http;
+using NerdBot.Messengers.GroupMe;
+using NUnit.Framework;
+
+namespace NerdBot.Tests
+{
+    [TestFixture]
+    class GroupMeMessenger_Tests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void SendMessage()
+        {
+            string json = @"";
+
+            var httpClientMock = new Mock<IHttpClient>();
+
+        }
+    }
+}
