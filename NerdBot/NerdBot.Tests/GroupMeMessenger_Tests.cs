d8db2b64970e167972ed8bf11445c81ee7757abd
jpann <ikonmobile@gmail.com>
2014-12-28 09:26:13 -0700

    GroupMeMessenger tests

--- NerdBot/NerdBot.Tests/GroupMeMessenger_Tests.cs
+++ NerdBot/NerdBot.Tests/GroupMeMessenger_Tests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Moq;
 using NerdBot.Http;
 using NerdBot.Messengers.GroupMe;
+using Ninject.Extensions.Logging;
 using NUnit.Framework;
 
 namespace NerdBot.Tests
@@ -13,19 +14,43 @@ namespace NerdBot.Tests
     [TestFixture]
     class GroupMeMessenger_Tests
     {
-        [SetUp]
-        public void SetUp()
-        {
-            
-        }
-
         [Test]
         public void SendMessage()
         {
-            string json = @"";
+            string url = "https://api.groupme.com/v3/bots/post";
+            string expectedJson = @"{""text"":""Message here"",""bot_id"":""BOT_ID""}";
+            
+            var httpClientMock = new Mock<IHttpClient>();
+            var loggerMock = new Mock<ILogger>();
+
+            var messenger = new GroupMeMessenger("BOT_ID", "BOT_NAME", new string[] {}, url, httpClientMock.Object, loggerMock.Object);
+
+            messenger.SendMessage("Message here");
+
+            httpClientMock.Verify(c => c.Post(url, expectedJson));
+        }
+
+        [Test]
+        public void SendMessage_Failure()
+        {
+            string url = "https://api.groupme.com/v3/bots/post";
+            string expectedJson = @"{""text"":""Message here"",""bot_id"":""BOT_ID""}";
 
             var httpClientMock = new Mock<IHttpClient>();
+            var loggerMock = new Mock<ILogger>();
 
+            // Mock failure
+            httpClientMock.Setup(c => c.Post(url, expectedJson))
+                .Throws(new System.Net.WebException("ERROR"));
+
+            // Mock error logging
+            loggerMock.Setup(c => c.Error(It.IsAny<Exception>(), "Error sending groupme message: ERROR"));
+
+            var messenger = new GroupMeMessenger("BOT_ID", "BOT_NAME", new string[] { }, url, httpClientMock.Object, loggerMock.Object);
+
+            bool actualResult = messenger.SendMessage("Message here");
+
+            Assert.False(actualResult);
         }
     }
 }
