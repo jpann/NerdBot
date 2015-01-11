aee9baf7979b5def9ac3991dd73dd4e4612e3708
jpann <ikonmobile@gmail.com>
2015-01-10 19:30:45 -0700

    Start implementing plugin to get card prices from TCGPlayer using DeckBrew API

---
+++ NerdBot/NerdBotCardPrices/DeckBrewPricePlugin.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NerdBot.Messengers;
+using NerdBot.Mtg;
+using NerdBot.Plugin;
+using NerdBotCardPrices.PriceFetchers;
+
+namespace NerdBotCardPrices
+{
+    public class DeckBrewPricePlugin : PluginBase
+    {
+
+        private const string cUrl = "https://api.deckbrew.com/mtg";
+
+        public override string Name
+        {
+            get { return "tcg Command"; }
+        }
+
+        public override string Description
+        {
+            get { return "Returns a card's TCGPlayer price using DeckBrew";  }
+        }
+
+        public DeckBrewPricePlugin()
+            : base()
+        {
+        }
+
+        public override void OnLoad()
+        {
+        }
+
+        public override void OnUnload()
+        {
+        }
+
+        public override async Task<bool> OnMessage(IMessage message, IMessenger messenger)
+        {
+            if (message == null)
+                throw new ArgumentNullException("message");
+
+            if (messenger == null)
+                throw new ArgumentNullException("messenger");
+
+            var command = this.mCommandParser.Parse(message.text);
+
+            // If there was no command, return
+            if (command == null)
+                return false;
+
+            // Parse command
+            if (command.Cmd.ToLower() == "tcg")
+            {
+                if (command.Arguments.Any())
+                {
+                    Card card = null;
+
+                    if (command.Arguments.Length == 1)
+                    {
+                        string name = command.Arguments[0];
+
+                        if (string.IsNullOrEmpty(name))
+                            return false;
+
+                        // Get card using only name
+                        card = await this.mStore.GetCard(name);
+                    }
+                    else if (command.Arguments.Length == 2)
+                    {
+                        string name = command.Arguments[0];
+                        string set = command.Arguments[1];
+
+                        if (string.IsNullOrEmpty(name))
+                            return false;
+
+                        if (string.IsNullOrEmpty(set))
+                            return false;
+
+                        // Get card using only name
+                        card = await this.mStore.GetCard(name, set);
+                    }
+
+                    if (card != null)
+                    {
+                        var tcg = new DeckBrewPriceFetcher(cUrl, base.HttpClient);
+                        string[] tcgPrice = tcg.GetPrice(card.MultiverseId);
+
+                        if (tcgPrice != null)
+                        {
+                            messenger.SendMessage(string.Format("{0} [{1}] - High: {2}; Median: {3}; Low: {4} - {5}",
+                                card.Name,
+                                card.SetName,
+                                tcgPrice[2],
+                                tcgPrice[1],
+                                tcgPrice[0],
+                                tcgPrice[3]));
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ NerdBot/NerdBotCardPrices/PriceFetchers/DeckBrewPriceFetcher.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NerdBot.Http;
+using Newtonsoft.Json;
+
+namespace NerdBotCardPrices.PriceFetchers
+{
+    public class DeckBrewPriceFetcher
+    {
+        private readonly string mUrl;
+        private readonly IHttpClient mHttpClient;
+
+        public DeckBrewPriceFetcher(string url, IHttpClient httpClient)
+        {
+            if (string.IsNullOrEmpty(url))
+                throw new ArgumentException("url");
+            
+            if (httpClient == null)
+                throw new ArgumentNullException("httpClient");
+
+            this.mUrl = url;
+            this.mHttpClient = httpClient;
+        }
+
+
+        //TODO This is lame
+        public string[] GetPrice(int multiverseId)
+        {
+            // Build the request url for this card
+            string url = string.Format("{0}/cards?multiverseid={1}", this.mUrl, multiverseId);
+
+            string jsonData = this.mHttpClient.GetAsJson(url).Result;
+
+            if (string.IsNullOrEmpty(jsonData))
+                return null;
+
+            List<DeckBrewCard> card = JsonConvert.DeserializeObject<List<DeckBrewCard>>(jsonData);
+            if (!card.Any())
+                return null;
+
+            if (!card[0].Editions.Any())
+                return null;
+
+            foreach (DeckBrewEdition edition in card[0].Editions)
+            {
+                if (edition.Price == null)
+                    continue;
+
+                double high = edition.Price.High;
+                double median = edition.Price.Median;
+                double low = edition.Price.Low;
+                string storeUrl = edition.Store_Url;
+
+                return new string[]
+                {
+                    low.ToString("0.00"),
+                    median.ToString("0.00"), 
+                    high.ToString("0.00"), 
+                    storeUrl
+                };
+            }
+
+            return null;
+        }
+    }
+
+    public class DeckBrewCard
+    {
+        public List<DeckBrewEdition> Editions { get; set; }
+    }
+
+    public class DeckBrewEdition
+    {
+        public int Multiverse_Id { get; set; }
+        public DeckBrewCardPrice Price { get; set; }
+        public string Store_Url { get; set; }
+    }
+
+    public class DeckBrewCardPrice
+    {
+        private double mHigh;
+        private double mMedian;
+        private double mLow;
+
+        public double High
+        {
+            get { return this.mHigh; }
+            set { this.mHigh = value / 100; }
+        }
+
+        public double Median
+        {
+            get { return this.mMedian; }
+            set { this.mMedian = value / 100; }
+        }
+
+        public double Low
+        {
+            get { return this.mLow; }
+            set { this.mLow = value / 100; }
+        }
+    }
+}
