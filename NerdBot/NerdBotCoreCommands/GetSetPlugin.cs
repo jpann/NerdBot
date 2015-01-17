1436e49b4b742195239eb4e4c15c62c54eb537b5
jpann <ikonmobile@gmail.com>
2015-01-16 23:08:00 -0700

    Start GetSetPlugin that returns set information

---
+++ NerdBot/NerdBotCoreCommands/GetSetPlugin.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NerdBot.Http;
+using NerdBot.Messengers;
+using NerdBot.Mtg;
+using NerdBot.Parsers;
+using NerdBot.Plugin;
+using NerdBot.UrlShortners;
+
+namespace NerdBotCoreCommands
+{
+    public class GetSetPlugin : PluginBase
+    {
+        public override string Name
+        {
+            get { return "set command"; }
+        }
+
+        public override string Description
+        {
+            get { return "Gets set information.";  }
+        }
+
+        public override string ShortDescription
+        {
+            get { return "Gets set information."; }
+        }
+
+        public override string Command
+        {
+            get { return "set"; }
+        }
+
+        public override string HelpCommand
+        {
+            get { return "help set"; }
+        }
+
+        public override string HelpDescription
+        {
+            get { return string.Format("{0}: HELP TEXT HERE", this.Command); }
+        }
+
+        public GetSetPlugin(
+                IMtgStore store,
+                ICommandParser commandParser,
+                IHttpClient httpClient,
+                IUrlShortener urlShortener)
+            : base(
+                store,
+                commandParser,
+                httpClient,
+                urlShortener)
+        {
+        }
+
+        public override void OnLoad()
+        {
+        }
+
+        public override void OnUnload()
+        {
+        }
+
+        public override async Task<bool> OnMessage(IMessage message, IMessenger messenger)
+        {
+            return false;
+        }
+
+        public override async Task<bool> OnCommand(Command command, IMessage message, IMessenger messenger)
+        {
+            if (command == null)
+                throw new ArgumentNullException("command");
+
+            if (message == null)
+                throw new ArgumentNullException("message");
+
+            if (messenger == null)
+                throw new ArgumentNullException("messenger");
+
+            if (command.Arguments.Any())
+            {
+                Set set = null;
+
+                if (command.Arguments.Length == 1)
+                {
+                    string name = command.Arguments[0];
+
+                    if (string.IsNullOrEmpty(name))
+                        return false;
+
+                    // Get card using only name
+                    if (name.Length == 3)
+                    {
+                        set = await this.Store.GetSetByCode(name);
+                    }
+                    else
+                    {
+                        set = await this.Store.GetSet(name);
+                    }
+                }
+
+                if (set != null)
+                {
+                    string msg =
+                        string.Format("Set '{0}' [{1}] in block '{2}' was released on {3} and contains {4} cards.", 
+                            set.Name,
+                            set.Code,
+                            set.Block,
+                            set.ReleasedOn.ToString("MM-dd-yyyy"),
+                            set.TotalQty);
+
+                    messenger.SendMessage(msg);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ NerdBot/NerdBotCoreCommandsPlugin_Tests/GetSetPlugin_Tests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NerdBot.Http;
+using NerdBot.Messengers;
+using NerdBot.Messengers.GroupMe;
+using NerdBot.Mtg;
+using NerdBot.Parsers;
+using NerdBot.UrlShortners;
+using NerdBotCoreCommands;
+using NUnit.Framework;
+
+namespace NerdBotCoreCommandsPlugin_Tests
+{
+    [TestFixture]
+    public class GetSetPlugin_Tests
+    {
+        private const string connectionString = "mongodb://localhost";
+        private const string databaseName = "mtgdb";
+
+        private IMtgStore mtgStore;
+        private GetSetPlugin plugin;
+
+        private Mock<ICommandParser> commandParserMock;
+        private Mock<IHttpClient> httpClientMock;
+        private Mock<IUrlShortener> urlShortenerMock;
+        private Mock<IMessenger> messengerMock;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            mtgStore = new MtgStore(connectionString, databaseName);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Setup ICommandParser Mocks
+            commandParserMock = new Mock<ICommandParser>();
+
+            // Setup IHttpClient Mocks
+            httpClientMock = new Mock<IHttpClient>();
+
+            // Setup IUrlShortener Mocks
+            urlShortenerMock = new Mock<IUrlShortener>();
+
+            // Setup IMessenger Mocks
+            messengerMock = new Mock<IMessenger>();
+
+            plugin = new GetSetPlugin(
+                mtgStore,
+                commandParserMock.Object,
+                httpClientMock.Object,
+                urlShortenerMock.Object);
+        }
+
+        [Test]
+        public void GetSet_ByName()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "set",
+                Arguments = new string[]
+                {
+                    "fallen empires"   
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            messengerMock.Verify(m => m.SendMessage("Set 'Fallen Empires' [FEM] in block '' was released on 11-01-1994 and contains 187 cards."));
+        }
+
+        //TODO Other tests
+    }
+}
