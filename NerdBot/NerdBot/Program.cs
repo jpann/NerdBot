f29bce037808a5ae1c8caf13e7d6aa311268bc70
jpann <ikonmobile@gmail.com>
2015-01-21 18:42:48 -0700

    Load listening host and port from config file.

--- NerdBot/NerdBot/Program.cs
+++ NerdBot/NerdBot/Program.cs
@@ -1,14 +1,21 @@
-﻿namespace NerdBot
+﻿using System.IO;
+using Nini.Config;
+
+namespace NerdBot
 {
     using System;
     using Nancy.Hosting.Self;
 
     class Program
     {
+        private static string mHostname = "localhost";
+        private static int mPort = 3579;
+
         static void Main(string[] args)
         {
-            var uri =
-                new Uri("http://localhost:3579");
+            LoadApplicationConfig();
+
+            var uri = new Uri(string.Format("http://{0}:{1}", mHostname, mPort));
 
             using (var host = new NancyHost(uri))
             {
@@ -17,6 +24,30 @@
                 Console.WriteLine("Your application is running on " + uri);
                 Console.WriteLine("Press any [Enter] to close the host.");
                 Console.ReadLine();
+            }
+        }
+
+        static void LoadApplicationConfig()
+        {
+            try
+            {
+                // Get configuration data
+                string configFile = Path.Combine(Environment.CurrentDirectory, "NerdBot.ini");
+                if (!File.Exists(configFile))
+                    throw new Exception("Configuration file 'NerdBot.ini' does not exist.");
+
+                IConfigSource source = new IniConfigSource(configFile);
+
+                string hostname = source.Configs["App"].Get("hostname");
+                if (!string.IsNullOrEmpty(hostname))
+                    mHostname = hostname;
+
+                int port = source.Configs["App"].GetInt("port");
+                mPort = port;
+            }
+            catch (Exception)
+            {
+                // Use defaults
             }
         }
     }
