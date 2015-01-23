e1942a9fe5a2203b81a6813eae766adc3711764e
jpann <ikonmobile@gmail.com>
2015-01-23 14:15:12 -0700

    Simple test for the RandomArtistPlugin.

---
+++ NerdBot/NerdBotCoreCommandsPlugin_Tests/RandomArtistPlugin_Tests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NerdBot.Http;
+using NerdBot.Messengers;
+using NerdBot.Messengers.GroupMe;
+using NerdBot.Mtg;
+using NerdBot.Parsers;
+using NerdBot.UrlShortners;
+using NerdBotCoreCommands;
+using NUnit.Framework;
+
+namespace NerdBotCoreCommandsPlugin_Tests
+{
+    [TestFixture]
+    class RandomArtistPlugin_Tests
+    {
+        private const string connectionString = "mongodb://localhost";
+        private const string databaseName = "mtgdb";
+
+        private IMtgStore mtgStore;
+        private RandomArtistPlugin plugin;
+
+        private Mock<ICommandParser> commandParserMock;
+        private Mock<IHttpClient> httpClientMock;
+        private Mock<IUrlShortener> urlShortenerMock;
+        private Mock<IMessenger> messengerMock;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            mtgStore = new MtgStore(connectionString, databaseName);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Setup ICommandParser Mocks
+            commandParserMock = new Mock<ICommandParser>();
+
+            // Setup IHttpClient Mocks
+            httpClientMock = new Mock<IHttpClient>();
+
+            // Setup IUrlShortener Mocks
+            urlShortenerMock = new Mock<IUrlShortener>();
+
+            // Setup IMessenger Mocks
+            messengerMock = new Mock<IMessenger>();
+
+            plugin = new RandomArtistPlugin(
+                mtgStore,
+                commandParserMock.Object,
+                httpClientMock.Object,
+                urlShortenerMock.Object);
+        }
+
+        [Test]
+        public void GetRandomArtist()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "randomartist",
+                Arguments = new string[]
+                {
+                    "edward%beard%"   
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            messengerMock.Verify(m => m.SendMessage(It.IsAny<string>()), Times.Once);
+        }
+    }
+}
