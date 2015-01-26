76f98634b6236b4ee3022f8640c32dd056bb4404
jpann <ikonmobile@gmail.com>
2015-01-26 12:51:05 -0700

    Test RandomCardPlugin

---
+++ NerdBot/NerdBotCoreCommandsPlugin_Tests/RandomCardPlugin_Tests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NerdBot.Http;
+using NerdBot.Messengers;
+using NerdBot.Messengers.GroupMe;
+using NerdBot.Mtg;
+using NerdBot.Parsers;
+using NerdBot.UrlShortners;
+using NerdBotCoreCommands;
+using NUnit.Framework;
+using SimpleLogging.Core;
+
+namespace NerdBotCoreCommandsPlugin_Tests
+{
+    [TestFixture]
+    class RandomCardPlugin_Tests
+    {
+        private const string connectionString = "mongodb://localhost";
+        private const string databaseName = "mtgdb";
+
+        private IMtgStore mtgStore;
+        private RandomCardPlugin plugin;
+
+        private Mock<ICommandParser> commandParserMock;
+        private Mock<IHttpClient> httpClientMock;
+        private Mock<IUrlShortener> urlShortenerMock;
+        private Mock<IMessenger> messengerMock;
+        private Mock<ILoggingService> loggingServiceMock;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            loggingServiceMock = new Mock<ILoggingService>();
+
+            mtgStore = new MtgStore(connectionString, databaseName, loggingServiceMock.Object);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Setup ICommandParser Mocks
+            commandParserMock = new Mock<ICommandParser>();
+
+            // Setup IHttpClient Mocks
+            httpClientMock = new Mock<IHttpClient>();
+
+            // Setup IUrlShortener Mocks
+            urlShortenerMock = new Mock<IUrlShortener>();
+
+            // Setup IMessenger Mocks
+            messengerMock = new Mock<IMessenger>();
+
+            plugin = new RandomCardPlugin(
+                mtgStore,
+                commandParserMock.Object,
+                httpClientMock.Object,
+                urlShortenerMock.Object);
+        }
+
+        [Test]
+        public void GetRandomCard()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "randomcard",
+                Arguments = new string[]
+                {
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            messengerMock.Verify(m => m.SendMessage(It.Is<string>(c => c.StartsWith("https://api.mtgdb.info/content/card_images/"))), Times.Once);
+        }
+    }
+}
