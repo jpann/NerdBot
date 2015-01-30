1e2613f57449370b95a6b59c35ab393036844d05
jpann <ikonmobile@gmail.com>
2015-01-30 11:00:14 -0700

    Start NerdBot

---
+++ NerdBot/NerdBot_PriceUpdater/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdBot_PriceUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
