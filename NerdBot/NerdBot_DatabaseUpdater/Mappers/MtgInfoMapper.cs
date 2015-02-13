bdb29b0153dd5077c7c44d5fd5281b1636e25ffb
jpann <ikonmobile@gmail.com>
2015-02-13 00:36:30 -0700

    Mapper classes for converting mtg data to domain data objects

--- NerdBot/NerdBot_DatabaseUpdater/Mappers/MtgInfoMapper.cs
+++ NerdBot/NerdBot_DatabaseUpdater/Mappers/MtgInfoMapper.cs
@@ -4,19 +4,89 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using NerdBot.Mtg;
+using NerdBot.Utilities;
 
 namespace NerdBot_DatabaseUpdater.Mappers
 {
     public class MtgInfoMapper : IMtgDataMapper<MtgDb.Info.Card, MtgDb.Info.CardSet>
     {
+        private readonly SearchUtility mSearchUtility;
+
+        public MtgInfoMapper(SearchUtility searchUtility)
+        {
+            if (searchUtility == null)
+                throw  new ArgumentNullException("searchUtility");
+
+            this.mSearchUtility = searchUtility;
+        }
+
         public Card GetCard(MtgDb.Info.Card source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            Card card = new Card();
+            card.Artist = source.Artist;
+            card.Cmc = source.ConvertedManaCost;
+            card.Colors = source.Colors;
+            card.Desc = source.Description;
+            card.Flavor = source.Flavor;
+            card.Img = source.CardImage;
+            card.ImgHires = source.ImageHiRes;
+            card.Loyalty = source.Loyalty.ToString();
+            card.MultiverseId = source.Id;
+            card.Name = source.Name;
+            card.Power = source.Power.ToString();
+            card.Rarity = source.Rarity;
+            card.RelatedCardId = source.RelatedCardId;
+
+            List<Ruling> rulings = new List<Ruling>();
+            foreach (MtgDb.Info.Ruling r in source.Rulings)
+            {
+                Ruling ruling = new Ruling()
+                {
+                    ReleasedOn = r.ReleasedAt,
+                    Rule = r.Rule
+                };
+
+                rulings.Add(ruling);
+            }
+            card.Rulings = rulings;
+
+            card.SearchName = this.mSearchUtility.GetSearchValue(source.Name);
+            card.SetId = source.CardSetId;
+            card.SetName = source.CardSetName;
+            card.SetSearchName = this.mSearchUtility.GetSearchValue(source.CardSetName);
+            card.SetNumber = source.SetNumber;
+            card.SubType = source.SubType;
+            card.Token = source.Token;
+            card.Toughness = source.Toughness.ToString();
+            card.Type = source.Type;
+
+            return card;
         }
 
         public Set GetSet(MtgDb.Info.CardSet source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            Set set = new Set();
+
+            set.BasicLandQty = source.BasicLand;
+            set.Block = source.Block;
+            set.Code = source.Id;
+            set.CommonQty = source.Common;
+            set.Desc = source.Description;
+            set.MythicQty = source.MythicRare;
+            set.Name = source.Name;
+            set.RareQty = source.Rare;
+            set.ReleasedOn = source.ReleasedAt;
+            set.Type = source.Type;
+            set.UncommonQty = source.Uncommon;
+            set.SearchName = this.mSearchUtility.GetSearchValue(source.Name);
+
+            return set;
         }
     }
 }
--- NerdBot/NerdBot_DatabaseUpdater/Mappers/MtgJsonMapper.cs
+++ NerdBot/NerdBot_DatabaseUpdater/Mappers/MtgJsonMapper.cs
@@ -2,22 +2,79 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using NerdBot.Mtg;
+using NerdBot.Utilities;
 using NerdBot_DatabaseUpdater.MtgData;
 
 namespace NerdBot_DatabaseUpdater.Mappers
 {
     class MtgJsonMapper : IMtgDataMapper<MtgJsonCard, MtgJsonSet>
     {
+        private readonly SearchUtility mSearchUtility;
+
+        public MtgJsonMapper(SearchUtility searchUtility)
+        {
+            if (searchUtility == null)
+                throw  new ArgumentNullException("searchUtility");
+
+            this.mSearchUtility = searchUtility;
+        }
+
         public Card GetCard(MtgJsonCard source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            Card card = new Card();
+
+            card.Artist = source.Artist;
+            card.Cmc = source.CMC;
+            card.Colors = source.Colors;
+            card.Cost = source.ManaCost;
+            card.Desc = source.Text;
+            card.Flavor = source.Flavor;
+            card.Img = string.Format("http://mtgimage.com/multiverseid/{0}.jpg", source.MultiverseId);
+            card.ImgHires = string.Format("http://mtgimage.com/multiverseid/{0}.jpg", source.MultiverseId);
+            card.Loyalty = source.Loyalty.ToString();
+            card.MultiverseId = Convert.ToInt32(source.MultiverseId);
+            card.Name = source.Name;
+            card.SearchName = this.mSearchUtility.GetSearchValue(source.Name);
+            card.SetId = source.SetId;
+            card.SetName = source.SetName;
+            card.SetSearchName = this.mSearchUtility.GetSearchValue(source.SetName);
+            card.Power = source.Power;
+            card.Rarity = source.Rarity;
+
+            if (source.SubTypes != null)
+                card.SubType = string.Join(" ", source.SubTypes.ToArray());
+
+            card.Token = false;
+            card.Toughness = source.Toughness;
+            card.Type = string.Join(" ", source.Types.ToArray());
+
+            return card;
         }
 
         public Set GetSet(MtgJsonSet source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            Set set = new Set();
+
+            set.Block = source.Block;
+            set.Type = source.Type;
+            set.Code = source.Code;
+            set.Name = source.Name;
+            set.ReleasedOn = source.ReleaseDate;
+            set.BasicLandQty = source.Cards.Count(c => c.Rarity.Contains("Basic Land"));
+            set.CommonQty = source.Cards.Count(c => c.Rarity.Contains("Common"));
+            set.MythicQty = source.Cards.Count(c => c.Rarity.Contains("Mythic"));
+            set.UncommonQty = source.Cards.Count(c => c.Rarity.Contains("Uncommon"));
+            set.RareQty = source.Cards.Count(c => c.Rarity.Contains("Rare"));
+            set.SearchName = this.mSearchUtility.GetSearchValue(source.Name);
+
+            return set;
         }
 
     }
