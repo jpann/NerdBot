b3dbd17a050859feda15a4264f593969f0dc6386
jpann <ikonmobile@gmail.com>
2015-02-28 17:25:58 -0700

    CommandParser tests

---
+++ NerdBot/NerdBot.Tests/CommandParser_Tests.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NerdBot.Parsers;
+using NUnit.Framework;
+
+namespace NerdBot.Tests
+{
+    [TestFixture]
+    class CommandParser_Tests
+    {
+        private ICommandParser commandParser;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            commandParser = new CommandParser();
+        }
+
+        #region Image Command Tests
+        [Test]
+        public void ImageCommand()
+        {
+            string message = "img spore burst";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(1, cmd.Arguments.Length);
+            Assert.AreEqual("spore burst", cmd.Arguments[0]);
+        }
+
+        [Test]
+        public void ImageCommand_WithWildcard()
+        {
+            string message = "img spore %";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(1, cmd.Arguments.Length);
+            Assert.AreEqual("spore %", cmd.Arguments[0]);
+        }
+
+        [Test]
+        public void ImageCommand_WithComma()
+        {
+            string message = "img shu yun, the silent tempest";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(1, cmd.Arguments.Length);
+            Assert.AreEqual("shu yun, the silent tempest", cmd.Arguments[0]);
+        }
+
+        [Test]
+        public void ImageCommand_WithCommaAndWildcard()
+        {
+            string message = "img shu yun, the % tempest";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(1, cmd.Arguments.Length);
+            Assert.AreEqual("shu yun, the % tempest", cmd.Arguments[0]);
+        }
+
+        [Test]
+        public void ImageCommand_WithSetName()
+        {
+            string message = "img conspiracy;spore burst";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(2, cmd.Arguments.Length);
+            Assert.AreEqual("conspiracy", cmd.Arguments[0]);
+            Assert.AreEqual("spore burst", cmd.Arguments[1]);
+        }
+
+        [Test]
+        public void ImageCommand_WithSetNameAndWildcard()
+        {
+            string message = "img conspiracy;spore %";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(2, cmd.Arguments.Length);
+            Assert.AreEqual("conspiracy", cmd.Arguments[0]);
+            Assert.AreEqual("spore %", cmd.Arguments[1]);
+        }
+
+        [Test]
+        public void ImageCommand_WithSetName_WithComma()
+        {
+            string message = "img fate reforged;shu yun, the silent tempest";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(2, cmd.Arguments.Length);
+            Assert.AreEqual("fate reforged", cmd.Arguments[0]);
+            Assert.AreEqual("shu yun, the silent tempest", cmd.Arguments[1]);
+        }
+
+        [Test]
+        public void ImageCommand_WithSetName_WithCommaAndWildcard()
+        {
+            string message = "img fate reforged;shu yun, the % tempest";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(2, cmd.Arguments.Length);
+            Assert.AreEqual("fate reforged", cmd.Arguments[0]);
+            Assert.AreEqual("shu yun, the % tempest", cmd.Arguments[1]);
+        }
+
+        [Test]
+        public void ImageCommand_WithSetCode()
+        {
+            string message = "img con;spore burst";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(2, cmd.Arguments.Length);
+            Assert.AreEqual("con", cmd.Arguments[0]);
+            Assert.AreEqual("spore burst", cmd.Arguments[1]);
+        }
+
+        [Test]
+        public void ImageCommand_WithSetCodeAndWildcard()
+        {
+            string message = "img con;spore %";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(2, cmd.Arguments.Length);
+            Assert.AreEqual("con", cmd.Arguments[0]);
+            Assert.AreEqual("spore %", cmd.Arguments[1]);
+        }
+
+        [Test]
+        public void ImageCommand_WithSetCode_WithComma()
+        {
+            string message = "img frf;shu yun, the silent tempest";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(2, cmd.Arguments.Length);
+            Assert.AreEqual("frf", cmd.Arguments[0]);
+            Assert.AreEqual("shu yun, the silent tempest", cmd.Arguments[1]);
+        }
+
+        [Test]
+        public void ImageCommand_WithSetCode_WithCommaAndWildcard()
+        {
+            string message = "img frf;shu yun, the % tempest";
+
+            var cmd = commandParser.Parse(message);
+
+            Assert.AreEqual("img", cmd.Cmd);
+            Assert.AreEqual(2, cmd.Arguments.Length);
+            Assert.AreEqual("frf", cmd.Arguments[0]);
+            Assert.AreEqual("shu yun, the % tempest", cmd.Arguments[1]);
+        }
+        #endregion
+    }
+}
