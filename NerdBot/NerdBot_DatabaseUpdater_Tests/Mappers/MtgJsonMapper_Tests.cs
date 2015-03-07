228f29b14d83d740a304da4c25bf569493e6ee31
jpann <ikonmobile@gmail.com>
2015-03-06 17:46:58 -0700

    Remove unused using

--- NerdBot/NerdBot_DatabaseUpdater_Tests/Mappers/MtgJsonMapper_Tests.cs
+++ NerdBot/NerdBot_DatabaseUpdater_Tests/Mappers/MtgJsonMapper_Tests.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Moq;
 using NerdBot.Utilities;
 using NerdBot_DatabaseUpdater.Mappers;
