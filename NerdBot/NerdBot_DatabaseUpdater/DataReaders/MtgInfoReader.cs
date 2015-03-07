82218fbf9d111286ad0f1cf3fed921f750852f26
jpann <ikonmobile@gmail.com>
2015-03-07 11:23:37 -0700

    Start implementing MtgInfoReader and tests

--- NerdBot/NerdBot_DatabaseUpdater/DataReaders/MtgInfoReader.cs
+++ NerdBot/NerdBot_DatabaseUpdater/DataReaders/MtgInfoReader.cs
@@ -1,22 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using MtgDb.Info.Driver;
 using NerdBot.Mtg;
+using NerdBot_DatabaseUpdater.Mappers;
+using SimpleLogging.Core;
 
 namespace NerdBot_DatabaseUpdater.DataReaders
 {
     public class MtgInfoReader : IMtgDataReader
     {
+        private readonly IMtgDataMapper<MtgDb.Info.Card, MtgDb.Info.CardSet> mDataMapper;
+        private readonly ILoggingService mLoggingService;
+        private readonly Db mMtgInfoDb;
+        private readonly string mSetId;
+
+        public MtgInfoReader(
+            string setId,
+            Db mtgInfoDb,
+            IMtgDataMapper<MtgDb.Info.Card, MtgDb.Info.CardSet> dataMapper,
+            ILoggingService loggingService)
+        {
+            if (string.IsNullOrEmpty(setId))
+                throw new ArgumentException("setName");
+
+            if (mtgInfoDb == null)
+                throw new ArgumentNullException("mtgInfoDb");
+
+            if (dataMapper == null)
+                throw new ArgumentNullException("dataMapper");
+
+            if (loggingService == null)
+                throw new ArgumentNullException("loggingService");
+
+            this.mSetId = setId;
+            this.mMtgInfoDb = mtgInfoDb;
+            this.mDataMapper = dataMapper;
+            this.mLoggingService = loggingService;
+        }
+
         public Set ReadSet()
         {
-            throw new NotImplementedException();
+            this.mLoggingService.Debug("Reading set '{0}'...", this.mSetId);
+            MtgDb.Info.CardSet setData = this.mMtgInfoDb.GetSet(this.mSetId);
+            this.mLoggingService.Debug("Read set!");
+
+            if (setData == null)
+                throw new Exception(string.Format("Set not found '{0}'.", this.mSetId));
+
+            this.mLoggingService.Debug("Mapping data...");
+            var set = this.mDataMapper.GetSet(setData);
+            this.mLoggingService.Debug("Data mapped for set '{0}'!", setData.Name);
+
+            return set;
         }
 
         public IEnumerable<Card> ReadCards()
         {
-            throw new NotImplementedException();
+            this.mLoggingService.Debug("Reading cards from set '{0}'...", this.mSetId);
+            //TODO This is returning NULL
+            MtgDb.Info.Card[] cardsData = this.mMtgInfoDb.GetSetCards(this.mSetId);
+            this.mLoggingService.Debug("Read cards!");
+
+            if (cardsData == null || !cardsData.Any())
+                throw new Exception(string.Format("Cards not found in set '{0}'.", this.mSetId));
+
+            this.mLoggingService.Trace("{0} cards found in set '{0}'.", 
+                cardsData.Length,
+                this.mSetId);
+
+            // Go through each card
+            foreach (MtgDb.Info.Card cardData in cardsData)
+            {
+                this.mLoggingService.Debug("Mapping data...");
+                var card = this.mDataMapper.GetCard(cardData);
+                this.mLoggingService.Debug("Data mapped for card '{0}'!", cardData.Name);
+
+                yield return card;
+            }
         }
     }
 }
---
+++ NerdBot/NerdBot_DatabaseUpdater_Tests/DataReaders/MtgInfoReader_Tests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Moq;
+using MtgDb.Info.Driver;
+using NerdBot.Mtg;
+using NerdBot.Utilities;
+using NerdBot_DatabaseUpdater.DataReaders;
+using NerdBot_DatabaseUpdater.Mappers;
+using NerdBot_DatabaseUpdater.MtgData;
+using NUnit.Framework;
+using SimpleLogging.Core;
+
+namespace NerdBot_DatabaseUpdater_Tests.DataReaders
+{
+    [TestFixture]
+    class MtgInfoReader_Tests
+    {
+        private MtgInfoReader reader;
+
+        private IMtgDataMapper<MtgDb.Info.Card, MtgDb.Info.CardSet> dataMapper;
+        private Mock<SearchUtility> searchUtilityMock;
+        private Mock<ILoggingService> loggingServiceMock;
+        private Db mtgInfoDb;
+
+        private string setId = "FRF";
+
+        public string GetSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        public string GetRegexSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Replace * and % with a regex '*' char
+            searchValue = searchValue.Replace("%", ".*");
+
+            // If the first character of the searchValue is not '*', 
+            // meaning the user does not want to do a contains search,
+            // explicitly use a starts with regex
+            if (!searchValue.StartsWith(".*"))
+            {
+                searchValue = "^" + searchValue;
+            }
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            // SearchUtility Mock
+            searchUtilityMock = new Mock<SearchUtility>();
+
+            searchUtilityMock.Setup(s => s.GetSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetSearchValue(s));
+
+            searchUtilityMock.Setup(s => s.GetRegexSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetRegexSearchValue(s));
+
+            // ILoggingService Mock
+            loggingServiceMock = new Mock<ILoggingService>();
+
+            dataMapper = new MtgInfoMapper(searchUtilityMock.Object);
+
+            mtgInfoDb = new Db();
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            reader = new MtgInfoReader(
+                setId,
+                mtgInfoDb,
+                dataMapper,
+                loggingServiceMock.Object);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+        #region ReadCards Tests
+        [Test]
+        public void ReadCards()
+        {
+            var cards = reader.ReadCards();
+
+            List<Card> actualCards = cards.ToList();
+
+            Assert.AreEqual(184, actualCards.Count());
+            Assert.AreEqual("Ugin, the Spirit Dragon", actualCards[0].Name);
+            Assert.AreEqual("Abzan Advantage", actualCards[1].Name);
+        }
+        #endregion
+
+        #region ReadSet Tests
+        [Test]
+        public void ReadSet()
+        {
+            var set = reader.ReadSet();
+
+            Assert.AreEqual("Fate Reforged", set.Name);
+        }
+        #endregion
+    }
+}
