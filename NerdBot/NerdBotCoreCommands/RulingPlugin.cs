d458a70128983922db3db62ca2a69796bc06e8f8
jpann <ikonmobile@gmail.com>
2015-03-09 09:35:20 -0600

    Implement RulingPlugin and tests

---
+++ NerdBot/NerdBotCoreCommands/RulingPlugin.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NerdBot.Http;
+using NerdBot.Messengers;
+using NerdBot.Mtg;
+using NerdBot.Mtg.Prices;
+using NerdBot.Parsers;
+using NerdBot.Plugin;
+using NerdBot.UrlShortners;
+
+namespace NerdBotCoreCommands
+{
+    public class RulingPlugin : PluginBase
+    {
+        private const string cRulingUrl = "http://silencio.ikonzeh.org:3579/ruling/{0}";
+
+        public override string Name
+        {
+            get { return "ruling command"; }
+        }
+
+        public override string Description
+        {
+            get { return "Returns a link that displays the specified card's rulings."; }
+        }
+
+        public override string ShortDescription
+        {
+            get { return "Returns a link that displays the specified card's rulings."; }
+        }
+
+        public override string Command
+        {
+            get { return "ruling"; }
+        }
+
+        public override string HelpCommand
+        {
+            get { return "help ruling"; }
+        }
+
+        public override string HelpDescription
+        {
+            get { return string.Format("{0} example usage: 'ruling spore clou%' or 'ruling fem;spore clou%' or 'ruling 'fallen empires;spore %loud'", this.Command); }
+        }
+
+        public RulingPlugin(
+                IMtgStore store,
+                ICardPriceStore priceStore,
+                ICommandParser commandParser,
+                IHttpClient httpClient,
+                IUrlShortener urlShortener)
+            : base(
+                store,
+                priceStore,
+                commandParser,
+                httpClient,
+                urlShortener)
+        {
+        }
+
+        public override void OnLoad()
+        {
+        }
+
+        public override void OnUnload()
+        {
+        }
+
+        public override async Task<bool> OnMessage(IMessage message, IMessenger messenger)
+        {
+            return false;
+        }
+
+        public override async Task<bool> OnCommand(Command command, IMessage message, IMessenger messenger)
+        {
+            if (command == null)
+                throw new ArgumentNullException("command");
+
+            if (message == null)
+                throw new ArgumentNullException("message");
+
+            if (messenger == null)
+                throw new ArgumentNullException("messenger");
+
+            if (command.Arguments.Any())
+            {
+                Card card = null;
+
+                if (command.Arguments.Length == 1)
+                {
+                    string name = command.Arguments[0];
+
+                    if (string.IsNullOrEmpty(name))
+                        return false;
+
+                    // Get card using only name
+                    card = await this.Store.GetCard(name);
+                }
+                else if (command.Arguments.Length == 2)
+                {
+                    string name = command.Arguments[1];
+                    string set = command.Arguments[0];
+
+                    if (string.IsNullOrEmpty(name))
+                        return false;
+
+                    if (string.IsNullOrEmpty(set))
+                        return false;
+
+                    // Get card using only name
+                    card = await this.Store.GetCard(name, set);
+                }
+
+                if (card != null)
+                {
+                    if (card.Rulings.Any())
+                    {
+                        string url = string.Format(cRulingUrl, card.MultiverseId);
+
+                        messenger.SendMessage(string.Format("Card ruling: {0}", url));
+                    }
+                    else
+                    {
+                        messenger.SendMessage(string.Format("Card '{0}' in set '{1}' has no rulings available.", card.Name, card.SetId));
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ NerdBot/NerdBotCoreCommandsPlugin_Tests/RulingPlugin_Tests.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Moq;
+using NerdBot.Http;
+using NerdBot.Messengers;
+using NerdBot.Messengers.GroupMe;
+using NerdBot.Mtg;
+using NerdBot.Mtg.Prices;
+using NerdBot.Parsers;
+using NerdBot.UrlShortners;
+using NerdBot.Utilities;
+using NerdBotCoreCommands;
+using NUnit.Framework;
+using SimpleLogging.Core;
+
+namespace NerdBotCoreCommandsPlugin_Tests
+{
+    [TestFixture]
+    public class RulingPlugin_Tests
+    {
+        private const string connectionString = "mongodb://localhost";
+        private const string databaseName = "mtgdb";
+
+        private IMtgStore mtgStore;
+        private RulingPlugin plugin;
+
+        private Mock<ICommandParser> commandParserMock;
+        private Mock<IHttpClient> httpClientMock;
+        private Mock<IUrlShortener> urlShortenerMock;
+        private Mock<IMessenger> messengerMock;
+        private Mock<ILoggingService> loggingServiceMock;
+        private Mock<ICardPriceStore> priceStoreMock;
+        private Mock<SearchUtility> searchUtilityMock;
+
+        public string GetSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        public string GetRegexSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Replace * and % with a regex '*' char
+            searchValue = searchValue.Replace("%", ".*");
+
+            // If the first character of the searchValue is not '*', 
+            // meaning the user does not want to do a contains search,
+            // explicitly use a starts with regex
+            if (!searchValue.StartsWith(".*"))
+            {
+                searchValue = "^" + searchValue;
+            }
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            loggingServiceMock = new Mock<ILoggingService>();
+            searchUtilityMock = new Mock<SearchUtility>();
+
+            searchUtilityMock.Setup(s => s.GetSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetSearchValue(s));
+
+            searchUtilityMock.Setup(s => s.GetRegexSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetRegexSearchValue(s));
+
+            mtgStore = new MtgStore(
+                connectionString, 
+                databaseName,
+                loggingServiceMock.Object,
+                searchUtilityMock.Object);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Setup ICardPriceStore Mocks
+            priceStoreMock = new Mock<ICardPriceStore>();
+
+            // Setup ICommandParser Mocks
+            commandParserMock = new Mock<ICommandParser>();
+
+            // Setup IHttpClient Mocks
+            httpClientMock = new Mock<IHttpClient>();
+
+            // Setup IUrlShortener Mocks
+            urlShortenerMock = new Mock<IUrlShortener>();
+
+            // Setup IMessenger Mocks
+            messengerMock = new Mock<IMessenger>();
+
+            plugin = new RulingPlugin(
+                mtgStore,
+                priceStoreMock.Object,
+                commandParserMock.Object,
+                httpClientMock.Object,
+                urlShortenerMock.Object);
+        }
+
+        [Test]
+        public void Ruling_ByName()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "ruling",
+                Arguments = new string[]
+                {
+                    "Fallen Angel"   
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled = 
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            messengerMock.Verify(m => m.SendMessage("Card ruling: http://silencio.ikonzeh.org:3579/ruling/26660"));
+        }
+
+        [Test]
+        public void Ruling_ByName_NameDoesntExist()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "ruling",
+                Arguments = new string[]
+                {
+                    "Bore Cloud"   
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            // Verify that the messenger's SendMessenger was never called 
+            //  because no message should be sent when a card was not found
+            messengerMock.Verify(m => m.SendMessage(It.IsAny<string>()), Times.Never);
+
+            Assert.False(handled);
+        }
+
+        [Test]
+        public void Ruling_ByNameAndSet()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "ruling",
+                Arguments = new string[]
+                {
+                    "Duel Decks Anthology, Divine vs. Demonic",
+                    "Fallen Angel"
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            messengerMock.Verify(m => m.SendMessage("Card ruling: http://silencio.ikonzeh.org:3579/ruling/394029"));
+        }
+
+        [Test]
+        public void Ruling_ByNameAndSet_NameDoesntExist()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "ruling",
+                Arguments = new string[]
+                {
+                    "Fallen Empires",
+                    "Bore Cloud"
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            // Verify that the messenger's SendMessenger was never called 
+            //  because no message should be sent when a card was not found
+            messengerMock.Verify(m => m.SendMessage(It.IsAny<string>()), Times.Never);
+
+            Assert.False(handled);
+        }
+
+        [Test]
+        public void Ruling_ByNameAndSet_SetDoesntExist()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "ruling",
+                Arguments = new string[]
+                {
+                    "Ballen Empires",
+                    "Spore Cloud"
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            // Verify that the messenger's SendMessenger was never called 
+            //  because no message should be sent when a card was not found
+            messengerMock.Verify(m => m.SendMessage(It.IsAny<string>()), Times.Never);
+
+            Assert.False(handled);
+        }
+
+        [Test]
+        public void Ruling_ByNameAndSetCode()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "ruling",
+                Arguments = new string[]
+                {
+                    "DD3_DVD",
+                    "Fallen Angel"
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            messengerMock.Verify(m => m.SendMessage("Card ruling: http://silencio.ikonzeh.org:3579/ruling/394029"));
+        }
+
+        [Test]
+        public void Ruling_ByNameAndSetCode_NameDoesntExist()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "ruling",
+                Arguments = new string[]
+                {
+                    "FEM",
+                    "Bore Cloud"
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            // Verify that the messenger's SendMessenger was never called 
+            //  because no message should be sent when a card was not found
+            messengerMock.Verify(m => m.SendMessage(It.IsAny<string>()), Times.Never);
+
+            Assert.False(handled);
+        }
+
+        [Test]
+        public void Ruling_ByNameAndSetCode_SetDoesntExist()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "ruling",
+                Arguments = new string[]
+                {
+                    "XXX",
+                    "Spore Cloud"
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            // Verify that the messenger's SendMessenger was never called 
+            //  because no message should be sent when a card was not found
+            messengerMock.Verify(m => m.SendMessage(It.IsAny<string>()), Times.Never);
+
+            Assert.False(handled);
+        }
+    }
+}
