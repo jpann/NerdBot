1a3c1ee041797760592f9ff9301bcb4f14aa4a8f
jpann <ikonmobile@gmail.com>
2017-08-17 13:59:40 -0600

    Add SearchPlugin unit test

---
+++ NerdBot/NerdBotCoreCommandsPlugin_Tests/SearchPlugin_Tests.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Moq;
+using NerdBot;
+using NerdBot.Parsers;
+using NerdBot.TestsHelper;
+using NerdBotCommon.Http;
+using NerdBotCommon.Messengers;
+using NerdBotCommon.Messengers.GroupMe;
+using NerdBotCommon.Mtg;
+using NerdBotCommon.Mtg.Prices;
+using NerdBotCommon.Parsers;
+using NerdBotCommon.UrlShortners;
+using NerdBotCommon.Utilities;
+using NerdBotCoreCommands;
+using NUnit.Framework;
+using SimpleLogging.Core;
+
+namespace NerdBotCoreCommandsPlugin_Tests
+{
+    [TestFixture]
+    public class SearchPlugin_Tests
+    {
+        private TestConfiguration testConfig;
+
+        private IMtgStore mtgStore;
+        private SearchPlugin plugin;
+
+        private Mock<ICommandParser> commandParserMock;
+        private Mock<IHttpClient> httpClientMock;
+        private Mock<IUrlShortener> urlShortenerMock;
+        private Mock<IMessenger> messengerMock;
+        private Mock<ILoggingService> loggingServiceMock;
+        private Mock<ICardPriceStore> priceStoreMock;
+        private Mock<SearchUtility> searchUtilityMock;
+
+        public string GetSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        public string GetRegexSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Replace * and % with a regex '*' char
+            searchValue = searchValue.Replace("%", ".*");
+
+            // If the first character of the searchValue is not '*', 
+            // meaning the user does not want to do a contains search,
+            // explicitly use a starts with regex
+            if (!searchValue.StartsWith(".*"))
+            {
+                searchValue = "^" + searchValue;
+            }
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            testConfig = new ConfigReader().Read();
+
+            loggingServiceMock = new Mock<ILoggingService>();
+            searchUtilityMock = new Mock<SearchUtility>();
+
+            searchUtilityMock.Setup(s => s.GetSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetSearchValue(s));
+
+            searchUtilityMock.Setup(s => s.GetRegexSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetRegexSearchValue(s));
+
+            mtgStore = new MtgStore(
+                testConfig.Url,
+                testConfig.Database, 
+                loggingServiceMock.Object,
+                searchUtilityMock.Object);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Setup ICardPriceStore Mocks
+            priceStoreMock = new Mock<ICardPriceStore>();
+
+            // Setup ICommandParser Mocks
+            commandParserMock = new Mock<ICommandParser>();
+
+            // Setup IHttpClient Mocks
+            httpClientMock = new Mock<IHttpClient>();
+
+            // Setup IUrlShortener Mocks
+            urlShortenerMock = new Mock<IUrlShortener>();
+
+            // Setup IMessenger Mocks
+            messengerMock = new Mock<IMessenger>();
+
+            var config = new BotConfig();
+            config.HostUrl = "http://localhost";
+
+            plugin = new SearchPlugin(
+                mtgStore,
+                priceStoreMock.Object,
+                commandParserMock.Object,
+                httpClientMock.Object,
+                urlShortenerMock.Object,
+                config);
+
+            plugin.LoggingService = loggingServiceMock.Object;
+        }
+
+        [Test]
+        public void Search_ByName()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "search",
+                Arguments = new string[]
+                {
+                    "Fallen Angel"   
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled = 
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            messengerMock.Verify(m => m.SendMessage(It.Is<string>(s => s.Contains("/search/"))));
+        }
+
+        [Test]
+        public void Search_NoNameProvided()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "search",
+                Arguments = new string[]
+                {
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+                plugin.OnCommand(
+                    cmd,
+                    msg,
+                    messengerMock.Object
+                ).Result;
+
+            // Verify that the messenger's SendMessenger was never called 
+            //  because no message should be sent when a card was not found
+            messengerMock.Verify(m => m.SendMessage(It.IsAny<string>()), Times.Never);
+
+            Assert.False(handled);
+        }
+    }
+}
