0891c3fcd1b167ad10b2ca9c93447860470f9ef3
jpann <ikonmobile@gmail.com>
2017-08-17 16:35:50 -0600

    Add unit tests for NerdBotGiphyPlugin

---
+++ NerdBot/NerdBotGiphyPlugin_Tests/GiphyFetcher_Tests.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Moq;
+using NerdBot.Parsers;
+using NerdBot.TestsHelper;
+using NerdBotCommon.Http;
+using NerdBotCommon.Messengers;
+using NerdBotCommon.Mtg;
+using NerdBotCommon.Mtg.Prices;
+using NerdBotCommon.UrlShortners;
+using NerdBotCommon.Utilities;
+using NerdBotGiphyPlugin;
+using NUnit.Framework;
+using SimpleLogging.Core;
+
+namespace NerdBotGiphyPlugin_Tests
+{
+    [TestFixture]
+    public class GiphyFetcher_Tests
+    {
+        private GiphyFetcher fetcher;
+        private Mock<IHttpClient> httpClientMock;
+        private Mock<ILoggingService> loggingServiceMock;
+
+        private string url = "http://localhost/{0}";
+        private string giphyUrl = @"https://media1.giphy.com/media/xT77XZrTKOxycjaYvK/giphy.gif";
+
+        private string giphydata = @"{
+  ""data"": {
+    ""images"": {
+      ""original"": {
+        ""url"": ""https://media1.giphy.com/media/xT77XZrTKOxycjaYvK/giphy.gif"",
+        ""width"": ""400"",
+        ""height"": ""250"",
+        ""size"": ""2535859"",
+        ""frames"": ""111"",
+        ""mp4"": ""https://media1.giphy.com/media/xT77XZrTKOxycjaYvK/giphy.mp4"",
+        ""mp4_size"": ""472711"",
+        ""webp"": ""https///media1.giphy.com/media/xT77XZrTKOxycjaYvK/giphy.webp"",
+        ""webp_size"": ""3193862""
+      }
+    }
+  }
+}";
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            httpClientMock = new Mock<IHttpClient>();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            string url = "http://localhost/{0}";
+
+            fetcher = new GiphyFetcher(url, httpClientMock.Object);
+        }
+
+        [Test]
+        public void GetGiphyGif()
+        {
+            string keyword = "cat";
+
+            var httpJsonTask = new TaskCompletionSource<string>();
+            httpJsonTask.SetResult(giphydata);
+
+            httpClientMock.Setup(h => h.GetAsJson(string.Format(url, keyword)))
+                .Returns(httpJsonTask.Task);
+
+            string actual = fetcher.GetGiphyGif(keyword).Result;
+
+            Assert.AreEqual(giphyUrl, actual);
+        }
+    }
+}
---
+++ NerdBot/NerdBotGiphyPlugin_Tests/GiphyPlugin_Tests.cs
@@ -0,0 +1,151 @@
+﻿using System.Text.RegularExpressions;
+using Moq;
+using NerdBot;
+using NerdBot.Parsers;
+using NerdBot.TestsHelper;
+using NerdBotCommon.Http;
+using NerdBotCommon.Messengers;
+using NerdBotCommon.Messengers.GroupMe;
+using NerdBotCommon.Mtg;
+using NerdBotCommon.Mtg.Prices;
+using NerdBotCommon.Parsers;
+using NerdBotCommon.UrlShortners;
+using NerdBotCommon.Utilities;
+using NerdBotGiphyPlugin;
+using NUnit.Framework;
+using SimpleLogging.Core;
+
+namespace NerdBotGiphyPlugin_Tests
+{
+    [TestFixture]
+    class GiphyPlugin_Tests
+    {
+        private TestConfiguration testConfig;
+
+        private IMtgStore mtgStore;
+        private GiphyPlugin plugin;
+
+        private Mock<ICommandParser> commandParserMock;
+        private IHttpClient httpClient;
+        private Mock<IUrlShortener> urlShortenerMock;
+        private Mock<IMessenger> messengerMock;
+        private Mock<ILoggingService> loggingServiceMock;
+        private Mock<ICardPriceStore> priceStoreMock;
+        private Mock<SearchUtility> searchUtilityMock;
+
+        public string GetSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        public string GetRegexSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Replace * and % with a regex '*' char
+            searchValue = searchValue.Replace("%", ".*");
+
+            // If the first character of the searchValue is not '*', 
+            // meaning the user does not want to do a contains search,
+            // explicitly use a starts with regex
+            if (!searchValue.StartsWith(".*"))
+            {
+                searchValue = "^" + searchValue;
+            }
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            testConfig = new ConfigReader().Read();
+
+            loggingServiceMock = new Mock<ILoggingService>();
+            searchUtilityMock = new Mock<SearchUtility>();
+
+            searchUtilityMock.Setup(s => s.GetSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetSearchValue(s));
+
+            searchUtilityMock.Setup(s => s.GetRegexSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetRegexSearchValue(s));
+
+            mtgStore = new MtgStore(
+                testConfig.Url,
+                testConfig.Database,
+                loggingServiceMock.Object,
+                searchUtilityMock.Object);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Setup ICardPriceStore Mocks
+            priceStoreMock = new Mock<ICardPriceStore>();
+
+            // Setup ICommandParser Mocks
+            commandParserMock = new Mock<ICommandParser>();
+
+            // Setup IHttpClient Mocks
+            httpClient = new SimpleHttpClient(loggingServiceMock.Object);
+
+            // Setup IMessenger Mocks
+            messengerMock = new Mock<IMessenger>();
+
+            // Setup IUrlShortener Mocks
+            urlShortenerMock = new Mock<IUrlShortener>();
+
+            plugin = new GiphyPlugin(
+                mtgStore,
+                priceStoreMock.Object,
+                commandParserMock.Object,
+                httpClient,
+                urlShortenerMock.Object,
+                new BotConfig());
+
+            plugin.LoggingService = loggingServiceMock.Object;
+        }
+
+        [Test]
+        public void Giphy_Call()
+        {
+            var cmd = new Command()
+            {
+                Cmd = "giphy",
+                Arguments = new string[]
+                {
+                    "geek"
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+                plugin.OnCommand(
+                    cmd,
+                    msg,
+                    messengerMock.Object
+                ).Result;
+
+            messengerMock.Verify(m => m.SendMessage(It.Is<string>(s => s.EndsWith("giphy.gif"))), Times.AtLeastOnce);
+        }
+    }
+}
