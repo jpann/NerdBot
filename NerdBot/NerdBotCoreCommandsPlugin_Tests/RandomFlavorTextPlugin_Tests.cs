2d493938b6399b6fb40174957c6511a0204f8863
jpann <ikonmobile@gmail.com>
2017-08-17 14:26:35 -0600

    Add unit tests for RandomFlavorTextPlugin

---
+++ NerdBot/NerdBotCoreCommandsPlugin_Tests/RandomFlavorTextPlugin_Tests.cs
@@ -0,0 +1,174 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Moq;
+using NerdBot;
+using NerdBot.Parsers;
+using NerdBotCommon.Http;
+using NerdBotCommon.Messengers;
+using NerdBotCommon.Messengers.GroupMe;
+using NerdBotCommon.Mtg;
+using NerdBotCommon.Mtg.Prices;
+using NerdBotCommon.Parsers;
+using NerdBotCommon.UrlShortners;
+using NerdBotCommon.Utilities;
+using NerdBotCoreCommands;
+using NUnit.Framework;
+using SimpleLogging.Core;
+
+namespace NerdBotCoreCommandsPlugin_Tests
+{
+    [TestFixture]
+    public class RandomFlavorTextPlugin_Tests
+    {
+        private RandomFlavorTextPlugin plugin;
+
+        private Mock<IMtgStore> mtgStoreMock;
+        private Mock<ICommandParser> commandParserMock;
+        private Mock<IHttpClient> httpClientMock;
+        private Mock<IUrlShortener> urlShortenerMock;
+        private Mock<IMessenger> messengerMock;
+        private Mock<ILoggingService> loggingServiceMock;
+        private Mock<ICardPriceStore> priceStoreMock;
+        private Mock<SearchUtility> searchUtilityMock;
+
+        public string GetSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        public string GetRegexSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Replace * and % with a regex '*' char
+            searchValue = searchValue.Replace("%", ".*");
+
+            // If the first character of the searchValue is not '*', 
+            // meaning the user does not want to do a contains search,
+            // explicitly use a starts with regex
+            if (!searchValue.StartsWith(".*"))
+            {
+                searchValue = "^" + searchValue;
+            }
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            loggingServiceMock = new Mock<ILoggingService>();
+            searchUtilityMock = new Mock<SearchUtility>();
+
+            searchUtilityMock.Setup(s => s.GetSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetSearchValue(s));
+
+            searchUtilityMock.Setup(s => s.GetRegexSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetRegexSearchValue(s));
+        }
+            
+        [SetUp]
+        public void SetUp()
+        {
+            // Setup IMtgStore Mocks
+            mtgStoreMock = new Mock<IMtgStore>();
+
+            // Setup ICardPriceStore Mocks
+            priceStoreMock = new Mock<ICardPriceStore>();
+
+            // Setup ICommandParser Mocks
+            commandParserMock = new Mock<ICommandParser>();
+
+            // Setup IHttpClient Mocks
+            httpClientMock = new Mock<IHttpClient>();
+
+            // Setup IUrlShortener Mocks
+            urlShortenerMock = new Mock<IUrlShortener>();
+
+            // Setup IMessenger Mocks
+            messengerMock = new Mock<IMessenger>();
+
+            plugin = new RandomFlavorTextPlugin(
+                mtgStoreMock.Object,
+                priceStoreMock.Object,
+                commandParserMock.Object,
+                httpClientMock.Object,
+                urlShortenerMock.Object,
+                new BotConfig());
+
+            plugin.LoggingService = loggingServiceMock.Object;
+
+            plugin.OnLoad();
+        }
+
+        [Test]
+        public void GetFlavor()
+        {
+            mtgStoreMock.Setup(s => s.GetRandomFlavorText())
+                .Returns(() => Task.FromResult("Flavor text."));
+
+            var cmd = new Command()
+            {
+                Cmd = "flavor",
+                Arguments = new string[]
+                {
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+            plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+                ).Result;
+
+            messengerMock.Verify(m => m.SendMessage(It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public void GetFlavor_NoText()
+        {
+            mtgStoreMock.Setup(s => s.GetRandomFlavorText())
+                .Returns(() => Task.FromResult(""));
+
+            var cmd = new Command()
+            {
+                Cmd = "flavor",
+                Arguments = new string[]
+                {
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled =
+                plugin.OnCommand(
+                    cmd,
+                    msg,
+                    messengerMock.Object
+                ).Result;
+
+            messengerMock.Verify(m => m.SendMessage(It.IsAny<string>()), Times.Never);
+        }
+    }
+}
