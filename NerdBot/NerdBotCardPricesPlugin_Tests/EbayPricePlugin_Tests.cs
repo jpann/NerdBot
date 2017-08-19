b6f6ccf736f51f8d16c553c7e9c5a8d6f62f4561
jpann <ikonmobile@gmail.com>
2017-08-19 09:48:28 -0600

    Add unit tests for EbayPricePlugin

---
+++ NerdBot/NerdBotCardPricesPlugin_Tests/EbayPricePlugin_Tests.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Moq;
+using NerdBot;
+using NerdBot.Parsers;
+using NerdBot.TestsHelper;
+using NerdBotCardPrices;
+using NerdBotCommon.Http;
+using NerdBotCommon.Messengers;
+using NerdBotCommon.Messengers.GroupMe;
+using NerdBotCommon.Mtg;
+using NerdBotCommon.Mtg.Prices;
+using NerdBotCommon.Parsers;
+using NerdBotCommon.UrlShortners;
+using NerdBotCommon.Utilities;
+using NUnit.Framework;
+using SimpleLogging.Core;
+
+namespace NerdBotCardPricesPlugin_Tests
+{
+    [TestFixture]
+    class EbayPricePlugin_Tests
+    {
+        private TestConfiguration testConfig;
+
+        private IMtgStore mtgStore;
+        private EbayPricePlugin plugin;
+
+        private Mock<ICommandParser> commandParserMock;
+        private IHttpClient httpClient;
+        private Mock<IUrlShortener> urlShortenerMock;
+        private Mock<IMessenger> messengerMock;
+        private Mock<ILoggingService> loggingServiceMock;
+        private Mock<ICardPriceStore> priceStoreMock;
+        private Mock<SearchUtility> searchUtilityMock;
+
+        public string GetSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        public string GetRegexSearchValue(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
+
+            string searchValue = text.ToLower();
+
+            // Replace * and % with a regex '*' char
+            searchValue = searchValue.Replace("%", ".*");
+
+            // If the first character of the searchValue is not '*', 
+            // meaning the user does not want to do a contains search,
+            // explicitly use a starts with regex
+            if (!searchValue.StartsWith(".*"))
+            {
+                searchValue = "^" + searchValue;
+            }
+
+            // Remove all non a-zA-Z0-9.^ characters
+            searchValue = rgx.Replace(searchValue, "");
+
+            // Remove all spaces
+            searchValue = searchValue.Replace(" ", "");
+
+            return searchValue;
+        }
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            testConfig = new ConfigReader().Read();
+
+            loggingServiceMock = new Mock<ILoggingService>();
+            searchUtilityMock = new Mock<SearchUtility>();
+
+            searchUtilityMock.Setup(s => s.GetSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetSearchValue(s));
+
+            searchUtilityMock.Setup(s => s.GetRegexSearchValue(It.IsAny<string>()))
+                .Returns((string s) => this.GetRegexSearchValue(s));
+
+            mtgStore = new MtgStore(
+                testConfig.Url,
+                testConfig.Database,
+                loggingServiceMock.Object,
+                searchUtilityMock.Object);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Setup ICardPriceStore Mocks
+            priceStoreMock = new Mock<ICardPriceStore>();
+
+            // Setup ICommandParser Mocks
+            commandParserMock = new Mock<ICommandParser>();
+
+            httpClient = new SimpleHttpClient(loggingServiceMock.Object);
+
+            // Setup IUrlShortener Mocks
+            urlShortenerMock = new Mock<IUrlShortener>();
+
+            // Setup IMessenger Mocks
+            messengerMock = new Mock<IMessenger>();
+
+            plugin = new EbayPricePlugin(
+                mtgStore,
+                priceStoreMock.Object,
+                commandParserMock.Object,
+                httpClient,
+                urlShortenerMock.Object,
+                new BotConfig());
+
+            plugin.LoggingService = loggingServiceMock.Object;
+        }
+
+        [Test]
+        public void GetPrice_ByName()
+        {
+            string name = "Inquisition of Kozilek";
+
+            var cmd = new Command()
+            {
+                Cmd = "ebay",
+                Arguments = new string[]
+                {
+                    name
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled = plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+            ).Result;
+
+            messengerMock.Verify(m => m.SendMessage(It.Is<string>(s => s.StartsWith(string.Format("The eBay Buy It Now price for '{0}' is", name)))),
+                Times.Once);
+        }
+
+        [Test]
+        public void GetPrice_ByNameSet()
+        {
+            string name = "Inquisition of Kozilek";
+            string set = "Rise of the Eldrazi";
+
+            var cmd = new Command()
+            {
+                Cmd = "ebay",
+                Arguments = new string[]
+                {
+                    set,
+                    name
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled = plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+            ).Result;
+
+            messengerMock.Verify(m => m.SendMessage(It.Is<string>(s => s.StartsWith(string.Format("The eBay Buy It Now price for '{0}' is", name)))),
+                Times.Once);
+        }
+
+        [Test]
+        public void GetPrice_ShortenedUrl()
+        {
+            string name = "Inquisition of Kozilek";
+            string url = "http://www.ebay.com/xxxxxxx";
+
+            urlShortenerMock.Setup(u => u.ShortenUrl(It.Is<string>(p => p.StartsWith("http://www.ebay.com/"))))
+                .Returns(() => url);
+
+            var cmd = new Command()
+            {
+                Cmd = "ebay",
+                Arguments = new string[]
+                {
+                    name
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled = plugin.OnCommand(
+                cmd,
+                msg,
+                messengerMock.Object
+            ).Result;
+
+            messengerMock.Verify(m => m.SendMessage(It.Is<string>(s => 
+            s.StartsWith(string.Format("The eBay Buy It Now price for '{0}' is", name)) &&
+            s.Contains(url))),
+                Times.Once);
+        }
+    }
+}
