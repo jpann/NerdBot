c84e74a371adf180649435656e4a5a8a7ba095ec
jpann <jpann@users.noreply.github.com>
2017-12-17 14:49:37 -0700

    Remove redundency for search methods in tests

--- NerdBot/NerdBot.Tests/PriceStore/EchoMtgPriceStore_FindAndModify_Tests.cs
+++ NerdBot/NerdBot.Tests/PriceStore/EchoMtgPriceStore_FindAndModify_Tests.cs
@@ -3,6 +3,7 @@ using System.Text.RegularExpressions;
 using MongoDB.Driver;
 using MongoDB.Driver.Builders;
 using Moq;
+using NerdBot.TestsHelper;
 using NerdBotCommon.Mtg.Prices;
 using NerdBotCommon.Utilities;
 using NUnit.Framework;
@@ -20,48 +21,7 @@ namespace NerdBot.Tests.PriceStore
         private ICardPriceStore priceStore;
         private Mock<ILoggingService> loggingServiceMock;
         private Mock<SearchUtility> searchUtilityMock;
-
-        public string GetSearchValue(string text)
-        {
-            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
-
-            string searchValue = text.ToLower();
-
-            // Remove all non a-zA-Z0-9.^ characters
-            searchValue = rgx.Replace(searchValue, "");
-
-            // Remove all spaces
-            searchValue = searchValue.Replace(" ", "");
-
-            return searchValue;
-        }
-
-        public string GetRegexSearchValue(string text)
-        {
-            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
-
-            string searchValue = text.ToLower();
-
-            // Replace * and % with a regex '*' char
-            searchValue = searchValue.Replace("%", ".*");
-
-            // If the first character of the searchValue is not '*', 
-            // meaning the user does not want to do a contains search,
-            // explicitly use a starts with regex
-            if (!searchValue.StartsWith(".*"))
-            {
-                searchValue = "^" + searchValue;
-            }
-
-            // Remove all non a-zA-Z0-9.^ characters
-            searchValue = rgx.Replace(searchValue, "");
-
-            // Remove all spaces
-            searchValue = searchValue.Replace(" ", "");
-
-            return searchValue;
-        }
-
+        
         #region Method to add test data
         private void AddTestData()
         {
@@ -80,7 +40,7 @@ namespace NerdBot.Tests.PriceStore
                 PriceLow = "$1.00",
                 PriceMid = "$2.00",
                 PriceFoil = "$3.00",
-                SearchName = this.GetSearchValue("Find Modify 1"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-1)
             };
@@ -93,7 +53,7 @@ namespace NerdBot.Tests.PriceStore
                 PriceLow = "$12.00",
                 PriceMid = "$22.00",
                 PriceFoil = "$32.00",
-                SearchName = this.GetSearchValue("Find Modify 1"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-1)
             };
@@ -106,7 +66,7 @@ namespace NerdBot.Tests.PriceStore
                 PriceLow = "$2.00",
                 PriceMid = "$3.00",
                 PriceFoil = "$4.00",
-                SearchName = this.GetSearchValue("Find Modify 2"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 2"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-1)
             };
@@ -122,7 +82,7 @@ namespace NerdBot.Tests.PriceStore
                 TotalCards = 1,
                 SetValue = "$2.00",
                 FoilSetValue = "$10.00",
-                SearchName = this.GetSearchValue("Find Modify 1"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-1)
             };
@@ -134,7 +94,7 @@ namespace NerdBot.Tests.PriceStore
                 TotalCards = 1,
                 SetValue = "$2.00",
                 FoilSetValue = "$10.00",
-                SearchName = this.GetSearchValue("Find Modify 2"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 2"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-1)
             };
@@ -146,7 +106,7 @@ namespace NerdBot.Tests.PriceStore
                 TotalCards = 1,
                 SetValue = "$4.00",
                 FoilSetValue = "$4.00",
-                SearchName = this.GetSearchValue("Find Modify 1"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-1)
             };
@@ -180,10 +140,10 @@ namespace NerdBot.Tests.PriceStore
             searchUtilityMock = new Mock<SearchUtility>();
 
             searchUtilityMock.Setup(s => s.GetSearchValue(It.IsAny<string>()))
-                .Returns((string s) => this.GetSearchValue(s));
+                .Returns((string s) => SearchHelper.GetSearchValue(s));
 
             searchUtilityMock.Setup(s => s.GetRegexSearchValue(It.IsAny<string>()))
-                .Returns((string s) => this.GetRegexSearchValue(s));
+                .Returns((string s) => SearchHelper.GetRegexSearchValue(s));
 
             priceStore = new EchoMtgPriceStore(
                 connectionString, 
@@ -215,7 +175,7 @@ namespace NerdBot.Tests.PriceStore
                 TotalCards = 1,
                 SetValue = "$10.00",
                 FoilSetValue = "$20.00",
-                SearchName = this.GetSearchValue("Find Modify 1"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now
             };
@@ -236,7 +196,7 @@ namespace NerdBot.Tests.PriceStore
                 TotalCards = 1,
                 SetValue = "$2.00",
                 FoilSetValue = "$10.00",
-                SearchName = this.GetSearchValue("Find Modify 3"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 3"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now
             };
@@ -257,7 +217,7 @@ namespace NerdBot.Tests.PriceStore
                 TotalCards = 1,
                 SetValue = "$2.00",
                 FoilSetValue = "$10.00",
-                SearchName = this.GetSearchValue("Find Modify 1"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now
             };
@@ -281,7 +241,7 @@ namespace NerdBot.Tests.PriceStore
                 PriceLow = "$10.00",
                 PriceMid = "$20.00",
                 PriceFoil = "$30.00",
-                SearchName = this.GetSearchValue("Find Modify 1"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now,
                 ImageUrl = "http://localhost/1000.jpg",
@@ -308,7 +268,7 @@ namespace NerdBot.Tests.PriceStore
                 PriceLow = "$10.00",
                 PriceMid = "$20.00",
                 PriceFoil = "$30.00",
-                SearchName = this.GetSearchValue("Find Modify 3"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 3"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now,
                 ImageUrl = "http://localhost/1000.jpg",
@@ -334,7 +294,7 @@ namespace NerdBot.Tests.PriceStore
                 PriceLow = "$10.00",
                 PriceMid = "$20.00",
                 PriceFoil = "$30.00",
-                SearchName = this.GetSearchValue("Find Modify 1"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now,
                 ImageUrl = "http://localhost/1000.jpg",
--- NerdBot/NerdBot.Tests/PriceStore/EchoMtgPriceStore_Remove_Tests.cs
+++ NerdBot/NerdBot.Tests/PriceStore/EchoMtgPriceStore_Remove_Tests.cs
@@ -3,6 +3,7 @@ using System.Text.RegularExpressions;
 using MongoDB.Driver;
 using MongoDB.Driver.Builders;
 using Moq;
+using NerdBot.TestsHelper;
 using NerdBotCommon.Mtg.Prices;
 using NerdBotCommon.Utilities;
 using NUnit.Framework;
@@ -22,48 +23,7 @@ namespace NerdBot.Tests.PriceStore
 
         private CardPrice cardPriceToRemove;
         private Mock<SearchUtility> searchUtilityMock;
-
-        public string GetSearchValue(string text)
-        {
-            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
-
-            string searchValue = text.ToLower();
-
-            // Remove all non a-zA-Z0-9.^ characters
-            searchValue = rgx.Replace(searchValue, "");
-
-            // Remove all spaces
-            searchValue = searchValue.Replace(" ", "");
-
-            return searchValue;
-        }
-
-        public string GetRegexSearchValue(string text)
-        {
-            Regex rgx = new Regex("[^a-zA-Z0-9.^*]");
-
-            string searchValue = text.ToLower();
-
-            // Replace * and % with a regex '*' char
-            searchValue = searchValue.Replace("%", ".*");
-
-            // If the first character of the searchValue is not '*', 
-            // meaning the user does not want to do a contains search,
-            // explicitly use a starts with regex
-            if (!searchValue.StartsWith(".*"))
-            {
-                searchValue = "^" + searchValue;
-            }
-
-            // Remove all non a-zA-Z0-9.^ characters
-            searchValue = rgx.Replace(searchValue, "");
-
-            // Remove all spaces
-            searchValue = searchValue.Replace(" ", "");
-
-            return searchValue;
-        }
-
+        
         #region Method to add test data
         private void AddTestData()
         {
@@ -82,7 +42,7 @@ namespace NerdBot.Tests.PriceStore
                 PriceLow = "$1.00",
                 PriceMid = "$2.00",
                 PriceFoil = "$3.00",
-                SearchName = this.GetSearchValue("Remove me 1"),
+                SearchName = SearchHelper.GetSearchValue("Remove me 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-5)
             };
@@ -95,7 +55,7 @@ namespace NerdBot.Tests.PriceStore
                 PriceLow = "$12.00",
                 PriceMid = "$22.00",
                 PriceFoil = "$32.00",
-                SearchName = this.GetSearchValue("Remove me 2"),
+                SearchName = SearchHelper.GetSearchValue("Remove me 2"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-6)
             };
@@ -108,7 +68,7 @@ namespace NerdBot.Tests.PriceStore
                 PriceLow = "$2.00",
                 PriceMid = "$3.00",
                 PriceFoil = "$4.00",
-                SearchName = this.GetSearchValue("Remove me 3"),
+                SearchName = SearchHelper.GetSearchValue("Remove me 3"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-7)
             };
@@ -121,7 +81,7 @@ namespace NerdBot.Tests.PriceStore
                 PriceLow = "$1.00",
                 PriceMid = "$2.00",
                 PriceFoil = "$3.00",
-                SearchName = this.GetSearchValue("Remove me X"),
+                SearchName = SearchHelper.GetSearchValue("Remove me X"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-2)
             };
@@ -138,7 +98,7 @@ namespace NerdBot.Tests.PriceStore
                 TotalCards = 1,
                 SetValue = "$2.00",
                 FoilSetValue = "$10.00",
-                SearchName = this.GetSearchValue("Find Modify 1"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-1)
             };
@@ -150,7 +110,7 @@ namespace NerdBot.Tests.PriceStore
                 TotalCards = 1,
                 SetValue = "$2.00",
                 FoilSetValue = "$10.00",
-                SearchName = this.GetSearchValue("Find Modify 2"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 2"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-1)
             };
@@ -162,7 +122,7 @@ namespace NerdBot.Tests.PriceStore
                 TotalCards = 1,
                 SetValue = "$4.00",
                 FoilSetValue = "$4.00",
-                SearchName = this.GetSearchValue("Find Modify 1"),
+                SearchName = SearchHelper.GetSearchValue("Find Modify 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-1)
             };
@@ -174,7 +134,7 @@ namespace NerdBot.Tests.PriceStore
                 TotalCards = 1,
                 SetValue = "$4.00",
                 FoilSetValue = "$4.00",
-                SearchName = this.GetSearchValue("Remove me 1"),
+                SearchName = SearchHelper.GetSearchValue("Remove me 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-2)
             };
@@ -210,10 +170,10 @@ namespace NerdBot.Tests.PriceStore
             searchUtilityMock = new Mock<SearchUtility>();
 
             searchUtilityMock.Setup(s => s.GetSearchValue(It.IsAny<string>()))
-                .Returns((string s) => this.GetSearchValue(s));
+                .Returns((string s) => SearchHelper.GetSearchValue(s));
 
             searchUtilityMock.Setup(s => s.GetRegexSearchValue(It.IsAny<string>()))
-                .Returns((string s) => this.GetRegexSearchValue(s));
+                .Returns((string s) => SearchHelper.GetRegexSearchValue(s));
 
             priceStore = new EchoMtgPriceStore(
                 connectionString, 
@@ -266,7 +226,7 @@ namespace NerdBot.Tests.PriceStore
                 TotalCards = 1,
                 SetValue = "$4.00",
                 FoilSetValue = "$4.00",
-                SearchName = this.GetSearchValue("Remove me 1"),
+                SearchName = SearchHelper.GetSearchValue("Remove me 1"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-5)
             };
@@ -332,7 +292,7 @@ namespace NerdBot.Tests.PriceStore
                 PriceLow = "$1.00",
                 PriceMid = "$2.00",
                 PriceFoil = "$3.00",
-                SearchName = this.GetSearchValue("Remove me"),
+                SearchName = SearchHelper.GetSearchValue("Remove me"),
                 Url = testDataTag,
                 LastUpdated = DateTime.Now.AddDays(-5)
             };
