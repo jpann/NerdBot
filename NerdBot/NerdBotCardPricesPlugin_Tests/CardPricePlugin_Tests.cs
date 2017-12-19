746777331edaff27b10162bd5eaf01628cfb5a88
jpann <jpann@users.noreply.github.com>
2017-12-19 15:34:34 -0700

    Add additional card price tests

--- NerdBot/NerdBotCardPricesPlugin_Tests/CardPricePlugin_Tests.cs
+++ NerdBot/NerdBotCardPricesPlugin_Tests/CardPricePlugin_Tests.cs
@@ -54,6 +54,169 @@ namespace NerdBotCardPricesPlugin_Tests
         }
 
         [Test]
+        public void GetPrice_ByName()
+        {
+            string name = "Inquisition of Kozilek";
+
+            // Setup mock to return price for this card by name
+            unitTestContext.PriceStoreMock.Setup(ps => ps.GetCardPrice(name))
+                .Returns(() => new CardPrice()
+                {
+                    Name = name,
+                    SearchName = SearchHelper.GetSearchValue(name),
+                    SetCode = "ROE",
+                    PriceFoil = "$100",
+                    PriceDiff = "10%"
+                });
+
+            var cmd = new Command()
+            {
+                Cmd = "tcg",
+                Arguments = new string[]
+                {
+                    "Inquisition of Kozilek"
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled = plugin.OnCommand(
+                cmd,
+                msg,
+                unitTestContext.MessengerMock.Object
+            ).Result;
+
+            unitTestContext.MessengerMock.Verify(m => 
+                m.SendMessage(It.Is<string>(s => s.StartsWith("Inquisition of Kozilek [ROE]"))),
+                    Times.Once);
+        }
+
+        [Test]
+        public void GetPrice_ByNameAndSet()
+        {
+            string name = "Inquisition of Kozilek";
+            string setCode = "MD1";
+
+            // Setup mock to return card price for card name and set
+            unitTestContext.PriceStoreMock.Setup(ps => ps.GetCardPrice(name, setCode))
+                .Returns(() => new CardPrice()
+                {
+                    Name = name,
+                    SearchName = SearchHelper.GetSearchValue(name),
+                    SetCode = "MD1",
+                    PriceFoil = "$100",
+                    PriceDiff = "10%"
+                });
+
+            var cmd = new Command()
+            {
+                Cmd = "tcg",
+                Arguments = new string[]
+                {
+                    "MD1",
+                    "Inquisition of Kozilek"
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled = plugin.OnCommand(
+                cmd,
+                msg,
+                unitTestContext.MessengerMock.Object
+            ).Result;
+
+            unitTestContext.MessengerMock.Verify(m => 
+                m.SendMessage(It.Is<string>(s => s.StartsWith("Inquisition of Kozilek [MD1]"))),
+                    Times.Once);
+        }
+
+        [Test]
+        public void GetPrice_NoPriceFound_NoRecord()
+        {
+            string name = "Inquisition of Kozilek";
+            string setCode = "MD1";
+
+            // Setup mock to return card price for card name
+            unitTestContext.PriceStoreMock.Setup(ps => ps.GetCardPrice(name, setCode))
+                .Returns(() => null);
+
+            var cmd = new Command()
+            {
+                Cmd = "tcg",
+                Arguments = new string[]
+                {
+                    "Inquisition of Kozilek"
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled = plugin.OnCommand(
+                cmd,
+                msg,
+                unitTestContext.MessengerMock.Object
+            ).Result;
+
+            unitTestContext.MessengerMock.Verify(m =>
+                    m.SendMessage(It.Is<string>(s => s.StartsWith("Price unavailable"))),
+                Times.Once);
+        }
+
+        [Test]
+        public void GetPrice_NoPriceFound_RecordWithNoData()
+        {
+            string name = "Inquisition of Kozilek";
+            string setCode = "MD1";
+
+            // Setup mock to return card price for card name and set
+            unitTestContext.PriceStoreMock.Setup(ps => ps.GetCardPrice(name))
+                .Returns(() => new CardPrice()
+                {
+                    Name = name,
+                    SearchName = SearchHelper.GetSearchValue(name),
+                    SetCode = "ROE",
+                    PriceLow = "",
+                    PriceMid = "",
+                    PriceFoil = "",
+                    PriceDiff = ""
+                });
+
+            unitTestContext.PriceStoreMock.Setup(ps => ps.GetCardPrice(name, setCode))
+                .Returns(() => new CardPrice()
+                {
+                    Name = name,
+                    SearchName = SearchHelper.GetSearchValue(name),
+                    SetCode = "ROE",
+                    PriceLow = "",
+                    PriceMid = "",
+                    PriceFoil = "",
+                    PriceDiff = ""
+                });
+
+            var cmd = new Command()
+            {
+                Cmd = "tcg",
+                Arguments = new string[]
+                {
+                    "Inquisition of Kozilek"
+                }
+            };
+
+            var msg = new GroupMeMessage();
+
+            bool handled = plugin.OnCommand(
+                cmd,
+                msg,
+                unitTestContext.MessengerMock.Object
+            ).Result;
+
+            unitTestContext.MessengerMock.Verify(m =>
+                    m.SendMessage(It.Is<string>(s => s.StartsWith("Price unavailable"))),
+                Times.Once);
+        }
+
+        [Test]
         public void GetPrice_SetNotAvailableFallbackToJustName()
         {
             string name = "Inquisition of Kozilek";
